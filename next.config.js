567fb46bb2e92bd7f61434e42c96473cd578de83
codetorso <pruthvirajthinks@gmail.com>
2024-03-29 17:46:56 +0530

    discord images used

--- next.config.js
+++ next.config.js
@@ -12,6 +12,11 @@ const config = {
             hostname: 'lh3.googleusercontent.com',
             port: '', 
             pathname: '/**', 
+        }, {
+            protocol: 'https', 
+            hostname: '"cdn.discordapp.com',
+            port: '', 
+            pathname: '/**',
         }]
     }
 };
